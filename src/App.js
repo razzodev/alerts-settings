1745acec99956fda182aff73d4c423d41aa63bc1
raz <razzo.des@gmail.com>
2021-09-14 20:13:51 +0300

    add(layout): header, main, footer /w basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="header">
+        <span className="title">Health Alerts</span>
+      </div>
+      <hr></hr>
+      <div id="main-wrapper"></div>
+      <hr></hr>
+      <div id="footer">
+        <span className="button save">SAVE</span>
+        <span className="button discard">Discard</span>
+      </div>
     </div>
   );
 }
