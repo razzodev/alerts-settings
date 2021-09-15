71cdca4726c7709d2471ad4532916c5987ecdc63
raz <razzo.des@gmail.com>
2021-09-15 12:43:59 +0300

    feat: impolement NestedField

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import AlertStore from "../Store";
 import NestedField from "./NestedField";
+import { edit } from "../utils";
 
 function MultInputs({ path, field, isActive }) {
   const updateInput = (e) => {
     AlertStore.update((s) => {
       s[path[0]][path[1]].value[field.name].value = e.target.value;
     });
-    console.log(e.target.value);
+    edit();
   };
 
-  const { active, name, isCheckbox, type, value, options } = field;
+  const { type, value, options } = field;
   return (
     <>
       {type === "select" && (
@@ -37,54 +38,75 @@ function Field({ label, field, path, textBefore, textAfter }) {
     AlertStore.update((s) => {
       s[path][field.name].active = !field.active;
     });
+    edit();
   };
 
   const updateInput = (e) => {
     AlertStore.update((s) => {
       s[path][field.name].value = e.target.value;
     });
+    edit();
   };
 
-  const { active, name, isCheckbox, type, value, options, children } = field;
+  const {
+    active,
+    name,
+    isCheckbox,
+    type,
+    value,
+    options,
+    children,
+    selectedChildren,
+  } = field;
   return (
-    <div>
+    <div className="field-wrapper">
       <div>
         <label>
           <strong>{label}</strong>
         </label>
       </div>
-      {isCheckbox && (
-        <input type="checkbox" checked={active} onChange={toggleActive} />
-      )}
-      <span className={!active && "disabled-text"}>{textBefore}</span>
-      {typeof value !== "object" ? (
-        <input
-          type={type}
-          value={value}
-          onChange={(e) => updateInput(e)}
-          disabled={!active}
-        />
-      ) : (
-        Object.keys(value).map((child) => (
-          <MultInputs
-            path={[path, name]}
-            field={value[child]}
-            isActive={active}
+      <div className="field-main">
+        {isCheckbox && (
+          <input type="checkbox" checked={active} onChange={toggleActive} />
+        )}
+        <span className={`${!active && "disabled-text"} span`}>
+          {textBefore}
+        </span>
+        {typeof value !== "object" ? (
+          <input
+            type={type}
+            value={value}
+            onChange={(e) => updateInput(e)}
+            disabled={!active}
           />
-        ))
-      )}
-
-      {type === "select" && (
-        <select>
-          {options.map((i) => (
-            <option key={i}>{i}</option>
-          ))}
-        </select>
-      )}
-      <span className={!active && "disabled-text"}>{textAfter}</span>
+        ) : (
+          Object.keys(value).map((child) => (
+            <MultInputs
+              path={[path, name]}
+              field={value[child]}
+              isActive={active}
+            />
+          ))
+        )}
+        {type === "select" && (
+          <select>
+            {options.map((i) => (
+              <option key={i}>{i}</option>
+            ))}
+          </select>
+        )}
+        <span className={`${!active && "disabled-text"} span`}>
+          {textAfter}
+        </span>
+      </div>
       {children &&
         Object.keys(children).map((child) => (
-          <NestedField path={[path, name]} field={children[child]} />
+          <NestedField
+            path={[path, name]}
+            field={children[child]}
+            count={selectedChildren}
+            isActive={active}
+          />
         ))}
     </div>
   );
