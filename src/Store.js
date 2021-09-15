88667544a1b8fdf96d7fe9e6e46dc105170ab227
raz <razzo.des@gmail.com>
2021-09-15 10:28:39 +0300

    add states

--- src/Store.js
+++ src/Store.js
@@ -1,28 +1,68 @@
-import Store from "pullstate";
+import { Store } from "pullstate";
 
 const AlertStore = new Store({
   isAdvanced: true,
   basic: {
     daily: {
+      name: "daily",
       active: true,
+      isCheckbox: true,
+      type: "time",
       value: "08:00",
     },
     monitor: {
+      name: "monitor",
+      active: true,
+      isCheckbox: false,
       value: 60,
+      type: "number",
     },
   },
   advanced: {
     diskStorage: {
+      name: "diskStorage",
       active: true,
-      day: "Sunday",
-      time: "08:00",
-      gb: {
-        active: true,
-        value: 1,
+      aggFunc: () => {},
+      isCheckbox: true,
+      value: {
+        day: {
+          name: "day",
+          type: "select",
+          value: "Sunday",
+          options: [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+          ],
+        },
+        time: {
+          name: "time",
+          type: "time",
+          value: "08:00",
+        },
       },
-      percentage: {
-        active: false,
-        value: 0.05,
+      children: {
+        GB: {
+          value: 1,
+          name: "GB",
+          type: "number",
+          textAfter: "GB",
+          active: true,
+          isCheckbox: true,
+          isLocked: true,
+        },
+        percentage: {
+          value: 0.05,
+          name: "percentage",
+          type: "number",
+          textAfter: "%",
+          active: false,
+          isCheckbox: true,
+          isLocked: false,
+        },
       },
     },
   },
