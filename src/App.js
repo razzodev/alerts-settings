9425d261e14e03a674a26a1c3e649235ea3da219
raz <razzo.des@gmail.com>
2021-09-15 10:28:13 +0300

    add: Field comp, populate add: AlertStore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import AlertStore from "./Store";
 import Card from "./components/Card";
+import Field from "./components/Field";
 import "./App.css";
 
 function App() {
+  const basic = AlertStore.useState((s) => s.basic);
+  const advanced = AlertStore.useState((s) => s.advanced);
   return (
     <div className="App">
       <div id="header">
@@ -11,13 +15,32 @@ function App() {
       <div id="main-wrapper">
         <Card title="Basic">
           <div>
-            <label>Daily health report</label>
+            <Field
+              label={"Daily health report"}
+              field={basic.daily}
+              path="basic"
+              textBefore="Send daily health report at:"
+            />
           </div>
           <div>
-            <label>Monitor service</label>
+            <Field
+              label={"Monitor service"}
+              field={basic.monitor}
+              path="basic"
+              textBefore="Wait"
+              textAfter="minutes before sending repetative email alerts"
+            />
           </div>
         </Card>
-        <Card title="Advanced"></Card>
+        <Card title="Advanced">
+          <Field
+            label={"Server data disk space"}
+            field={advanced.diskStorage}
+            path="advanced"
+            textBefore="Send daily health report at:"
+            textAfter="when disk space has dropped below:"
+          />
+        </Card>
       </div>
       <hr className="main-hr"></hr>
       <div id="footer">
